0a2aa57663ad60cf3c92349b2d6557b4c06ebd3d
ragingo <save.the.world.crush@gmail.com>
2022-12-25 23:00:17 +0900

    [Card] CardActions 作成

--- RagiSwiftUIComponents/RagiSwiftUIComponents/Components/Surfaces/Card/Views/CardActions.swift
+++ RagiSwiftUIComponents/RagiSwiftUIComponents/Components/Surfaces/Card/Views/CardActions.swift
@@ -10,16 +10,72 @@ import SwiftUI
 /// https://m3.material.io/components/cards/guidelines
 /// https://mui.com/material-ui/react-card/
 /// https://mui.com/material-ui/api/card-actions/
-public struct CardActions: View {
-    public init() {}
+public struct CardActions<Content: View>: View {
+    private let content: () -> Content
+
+    public init(content: @escaping () -> Content) {
+        self.content = content
+    }
 
     public var body: some View {
-        EmptyView()
+        content()
+            .padding(8)
     }
 }
 
 struct CardActions_Previews: PreviewProvider {
+    struct PreviewView: View {
+        @State private var isLiked = false
+        @State private var isExpanded = false
+
+        var body: some View {
+            CardActions {
+                HStack {
+                    likeButton
+                    shareButton
+                    Spacer()
+                    showMoreButton
+                }
+            }
+            .background(.gray.opacity(0.4))
+        }
+
+        var likeButton: some View {
+            Button {
+                withAnimation {
+                    isLiked.toggle()
+                }
+            } label: {
+                Image(systemName: isLiked ? "heart.fill" : "heart")
+                    .scaleEffect(1.2)
+            }
+            .buttonStyle(.plain)
+        }
+
+        var shareButton: some View {
+            Button {
+            } label: {
+                Image(systemName: "square.and.arrow.up")
+                    .scaleEffect(1.2)
+            }
+            .buttonStyle(.plain)
+        }
+
+        var showMoreButton: some View {
+            Button {
+                withAnimation {
+                    isExpanded.toggle()
+                }
+            } label: {
+                Image(systemName: "chevron.down")
+                    .scaleEffect(1.2)
+                    .rotationEffect(.degrees(isExpanded ? 180 : 0))
+            }
+            .buttonStyle(.plain)
+        }
+    }
+
     static var previews: some View {
-        CardActions()
+        PreviewView()
     }
 }
