9abf2c826052a710aab4822c4082ddb75cf84385
ragingo <save.the.world.crush@gmail.com>
2022-12-25 21:59:45 +0900

    [Card] CardContent 作成

--- RagiSwiftUIComponents/RagiSwiftUIComponents/Components/Surfaces/Card/Views/CardContent.swift
+++ RagiSwiftUIComponents/RagiSwiftUIComponents/Components/Surfaces/Card/Views/CardContent.swift
@@ -10,16 +10,24 @@ import SwiftUI
 /// https://m3.material.io/components/cards/guidelines
 /// https://mui.com/material-ui/react-card/
 /// https://mui.com/material-ui/api/card-content/
-public struct CardContent: View {
-    public init() {}
+public struct CardContent<Content: View>: View {
+    private let content: () -> Content
+
+    public init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
 
     public var body: some View {
-        EmptyView()
+        content()
+            .padding(16)
     }
 }
 
 struct CardContent_Previews: PreviewProvider {
     static var previews: some View {
-        CardContent()
+        CardContent {
+            Text("content")
+        }
+        .background(.gray.opacity(0.4))
     }
 }
