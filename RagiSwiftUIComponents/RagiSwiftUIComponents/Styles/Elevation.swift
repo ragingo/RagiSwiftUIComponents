09f2cd614f9fc5660cc7a78153ca55995d855fac
ragingo <save.the.world.crush@gmail.com>
2022-12-27 23:20:05 +0900

    [Card] "次のelevation" を用意してみる

--- RagiSwiftUIComponents/RagiSwiftUIComponents/Styles/Elevation.swift
+++ RagiSwiftUIComponents/RagiSwiftUIComponents/Styles/Elevation.swift
@@ -15,4 +15,21 @@ public enum Elevation: Int {
     case level3 = 6
     case level4 = 8
     case level5 = 12
+
+    public var next: Elevation {
+        switch self {
+        case .level0:
+            return .level1
+        case .level1:
+            return .level2
+        case .level2:
+            return .level3
+        case .level3:
+            return .level4
+        case .level4:
+            return .level5
+        case .level5:
+            return .level0
+        }
+    }
 }
