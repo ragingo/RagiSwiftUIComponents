984fc85d526d4f32789c3649a8c3e00d9b8f107f
ragingo <save.the.world.crush@gmail.com>
2022-12-29 14:15:10 +0900

    [Slider] track の Y 座標を修正

--- RagiSwiftUIComponents/RagiSwiftUIComponents/Components/Slider/Slider.swift
+++ RagiSwiftUIComponents/RagiSwiftUIComponents/Components/Slider/Slider.swift
@@ -10,18 +10,19 @@ import SwiftUI
 public struct Slider: View {
     @State private var handlePositionX: CGFloat = 0
     private let sliderTrackSpaceName = UUID()
+    private let trackHeight: CGFloat = 4
 
     public var body: some View {
         GeometryReader { geometry in
-            SliderTrack(size: .init(width: geometry.size.width, height: 4))
+            SliderTrack(size: .init(width: geometry.size.width, height: trackHeight))
                 .overlay {
                     SliderHandle {
                         Circle()
                             .fill(.white)
                             .frame(width: 20, height: 20)
                             .shadow(radius: 4)
-                            .position(x: handlePositionX)
                     }
+                    .position(x: handlePositionX, y: trackHeight * 0.5)
                     .gesture(DragGesture(coordinateSpace: .named(sliderTrackSpaceName))
                         .onChanged { value in
                             let x = min(max(value.location.x, 0), geometry.size.width)
