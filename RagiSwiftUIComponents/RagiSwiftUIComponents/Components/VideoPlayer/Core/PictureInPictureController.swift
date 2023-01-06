6efe1355cab6723c908a762160c3f7db1a3d95c7
ragingo <save.the.world.crush@gmail.com>
2023-01-06 15:06:38 +0900

    [VideoPlayer] PiP の再生制御系ボタンを隠す
    切り替え対応は後日やるかも

--- RagiSwiftUIComponents/RagiSwiftUIComponents/Components/VideoPlayer/Core/PictureInPictureController.swift
+++ RagiSwiftUIComponents/RagiSwiftUIComponents/Components/VideoPlayer/Core/PictureInPictureController.swift
@@ -10,7 +10,7 @@ import class AVKit.AVPictureInPictureController
 import protocol AVKit.AVPictureInPictureControllerDelegate
 import Combine
 
-final class PictureInPictureController: NSObject, AVPictureInPictureControllerDelegate {
+final class PictureInPictureController: NSObject {
     enum Properties {
         case isActive(value: Bool)
         case isPossible(value: Bool)
@@ -44,6 +44,10 @@ final class PictureInPictureController: NSObject, AVPictureInPictureControllerDe
         self.pictureInPictureController = controller
         controller.canStartPictureInPictureAutomaticallyFromInline = true
         controller.delegate = self
+        // 前後スキップボタンを隠すなら true にする
+        controller.requiresLinearPlayback = true
+        // 再生/一時停止ボタンを隠すなら controlsStyle を inline にする
+        controller.setValue(AVPlayerViewControlsStyle.inline.rawValue, forKey: "controlsStyle")
 
         keyValueObservations += observeProperties()
     }
@@ -63,6 +67,18 @@ final class PictureInPictureController: NSObject, AVPictureInPictureControllerDe
         pictureInPictureController?.stopPictureInPicture()
     }
 
+    @KVOBuilder
+    private func observeProperties() -> [KVOBuilder.Element] {
+        pictureInPictureController?.observe(\.isPictureInPictureActive, options: [.initial, .new]) { [weak self] controller, change in
+            self?._properties.send(.isActive(value: change.newValue ?? false))
+        }
+        pictureInPictureController?.observe(\.isPictureInPicturePossible, options: [.initial, .new]) { [weak self] controller, change in
+            self?._properties.send(.isPossible(value: change.newValue ?? false))
+        }
+    }
+}
+
+extension PictureInPictureController: AVPictureInPictureControllerDelegate {
     func pictureInPictureControllerWillStartPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
         _properties.send(.starting)
     }
@@ -78,14 +94,44 @@ final class PictureInPictureController: NSObject, AVPictureInPictureControllerDe
     func pictureInPictureControllerDidStopPictureInPicture(_ pictureInPictureController: AVPictureInPictureController) {
         _properties.send(.stopped)
     }
+}
 
-    @KVOBuilder
-    private func observeProperties() -> [KVOBuilder.Element] {
-        pictureInPictureController?.observe(\.isPictureInPictureActive, options: [.initial, .new]) { [weak self] controller, change in
-            self?._properties.send(.isActive(value: change.newValue ?? false))
+// macOS 向けの AVPlayerViewControlsStyle を iOS で使いたいから自分で定義
+// https://learn.microsoft.com/ja-jp/dotnet/api/avkit.avplayerviewcontrolsstyle?view=xamarin-ios-sdk-12
+private enum AVPlayerViewControlsStyle: RawRepresentable {
+    init?(rawValue: Int) {
+        switch rawValue {
+        case 0:
+            self = .none
+        case 1:
+            self = .inline
+        case 2:
+            self = .floating
+        case 3:
+            self = .minimal
+        default:
+            return nil
         }
-        pictureInPictureController?.observe(\.isPictureInPicturePossible, options: [.initial, .new]) { [weak self] controller, change in
-            self?._properties.send(.isPossible(value: change.newValue ?? false))
+    }
+
+    case none
+    case inline
+    case floating
+    case minimal
+    case `default`
+
+    var rawValue: Int {
+        switch self {
+        case .none:
+            return 0
+        case .inline:
+            return 1
+        case .floating:
+            return 2
+        case .minimal:
+            return 3
+        case .default:
+            return 1
         }
     }
 }
