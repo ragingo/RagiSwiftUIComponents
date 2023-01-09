26bbd8381ca2039cdee771a51777ff14643fda84
ragingo <save.the.world.crush@gmail.com>
2023-01-09 23:52:52 +0900

    [VideoPlayer] 再生速度変更UI追加

--- RagiSwiftUIComponents/RagiSwiftUIComponentsExampleApp/Views/VideoPlayer/Detail/VideoPlayerOperationLayer.swift
+++ RagiSwiftUIComponents/RagiSwiftUIComponentsExampleApp/Views/VideoPlayer/Detail/VideoPlayerOperationLayer.swift
@@ -21,6 +21,8 @@ struct VideoPlayerOperationLayer: View {
     @Binding var isSliderHandleDragging: Bool
     @Binding var sliderValue: Double
 
+    @State private var rateSliderValue = 0.0
+
     var body: some View {
         ZStack {
             baseLayer
@@ -43,6 +45,12 @@ struct VideoPlayerOperationLayer: View {
                     Spacer()
                 }
 
+                HStack {
+                    Spacer()
+                    rateSlider
+                        .frame(width: 100)
+                }
+
                 VideoPlayerSlider(
                     position: $sliderValue,
                     duration: $duration,
@@ -60,6 +68,20 @@ struct VideoPlayerOperationLayer: View {
         HStack {
             playButton
         }
+    }
+
+    private var rateSlider: some View {
+        RagiSwiftUIComponents.Slider(value: $rateSliderValue)
+            .sliderStyle(RateSliderStyle())
+            .onChange(of: rateSliderValue) { _ in
+                let rate = Float((rateSliderValue * 100.0 * 5.0).rounded(.down)) / 100.0
+                playerCommand.send(.rate(value: rate))
+            }
+            .overlay {
+                let rate = Float((rateSliderValue * 100.0 * 5.0).rounded(.down)) / 100.0
+                Text("x \(rate, specifier: "%.2f")")
+                    .offset(y: -20)
+            }
     }
 
     private var playButton: some View {
@@ -111,4 +133,42 @@ private func formatTime(seconds: Double) -> String {
         return String(format: "%03d:%02d:%02d", h, m, s)
     }
     return String(format: "%02d:%02d", m, s)
+}
+
+private struct RateSliderStyle: SliderStyle {
+    private static let trackHeight: CGFloat = 4
+
+    public func makeActiveTrack(configuration: SliderTrackConfiguration) -> some View {
+        configuration.content
+            .frame(height: Self.trackHeight)
+            .foregroundColor(Color(uiColor: .systemBlue))
+    }
+
+    public func makeInactiveTrack(configuration: SliderTrackConfiguration) -> some View {
+        configuration.content
+            .frame(height: Self.trackHeight)
+            .foregroundColor(Color(uiColor: .systemGray6))
+    }
+
+    public func makeHandle(configuration: SliderHandleConfiguration) -> some View {
+        SliderHandle {
+            Image(systemName: "speedometer")
+                .resizable()
+                .frame(width: 20, height: 20)
+                .clipShape(Circle())
+                .shadow(elevation: .level1, cornerRadius: 10)
+                .background(
+                    Circle()
+                        .fill(.gray.opacity(0.2))
+                        .scaleEffect(2.0)
+                        .opacity(configuration.isPressed ? 1 : 0)
+                        .animation(.default, value: configuration.isPressed)
+                )
+        }
+        .offset(y: Self.trackHeight * 0.5)
+    }
+
+    public func makeBody(configuration: SliderConfiguration) -> some View {
+        configuration.content
+    }
 }
